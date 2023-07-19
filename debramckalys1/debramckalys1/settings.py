bd37e9f2798e75421c4e3995af781b206ac6695e
Opeyemi Olutayo <ope@nultan.com>
2023-07-19 23:47:29 +0000

    Updated settings.py

--- debramckalys1/debramckalys1/settings.py
+++ debramckalys1/debramckalys1/settings.py
@@ -134,7 +134,12 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
