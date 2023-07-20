4759ceda66c7127d88c6a353bac5574490da3f28
Opeyemi Olutayo <ope@nultan.com>
2023-07-20 01:16:41 +0000

    Updated settings.py

--- debramckalys1/debramckalys1/settings.py
+++ debramckalys1/debramckalys1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', ''), 'www.debramckalys,com', 'debramckalys.com']
 
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #STATICFILES_DIRS = (
 #    BASE_DIR / 'static',
