c7edbf70b79c4820a6508d6bc8cd0b5dc685c5fe
Opeyemi Olutayo <ope@nultan.com>
2023-08-01 02:45:04 +0000

    Updated settings.py

--- debramckalys1/debramckalys1/settings.py
+++ debramckalys1/debramckalys1/settings.py
@@ -173,5 +173,15 @@ WHITENOISE_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT =  BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+#Azure Blob Storage connection
+
+DEFAULT_FILE_STORAGE = 'core.azure_storage.AzureMediaStorage'
+AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
+AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/media/'
+
+#MEDIA_ROOT =  BASE_DIR / 'media'
+#MEDIA_URL = '/media/'
